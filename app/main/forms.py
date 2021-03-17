3fc10a86ca342a1d14a55f7bb88d89220cf6f56b
Mauricio Andre Cinelli <mauricio.andre.cinelli@gmail.com>
2021-03-17 09:45:30 -0300

    Removing Letter from template radio buttons

--- app/main/forms.py
+++ app/main/forms.py
@@ -2199,7 +2199,6 @@ class TemplateAndFoldersSelectionForm(Form):
         self.add_template_by_template_type.choices = list(filter(None, [
             ('email', 'Email') if 'email' in available_template_types else None,
             ('sms', 'Text message') if 'sms' in available_template_types else None,
-            ('letter', 'Letter') if 'letter' in available_template_types else None,
             ('broadcast', 'Broadcast') if 'broadcast' in available_template_types else None,
             ('copy-existing', 'Copy an existing template') if allow_adding_copy_of_template else None,
         ]))
